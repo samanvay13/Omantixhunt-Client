2c6b3af59d5e1c5385d291ed806c4fd9bed5fde8
samanvay13 <tripathisamanvay13@gmail.com>
2024-05-01 05:22:12 +0530

    notification data

--- src/app/notifications-page/notifications-page.component.ts
+++ src/app/notifications-page/notifications-page.component.ts
@@ -1,135 +1,66 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { RouterLink } from '@angular/router';
-import { SelectionModel } from '@angular/cdk/collections';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { CustomizerSettingsService } from '../customizer-settings/customizer-settings.service';
+import { HttpClient } from '@angular/common/http';
+import { MatDialog } from '@angular/material/dialog';
+import { APIService } from '../apiservice';
 
 @Component({
     selector: 'app-notifications-page',
     standalone: true,
-    imports: [MatCardModule, MatMenuModule, MatButtonModule, RouterLink, MatTableModule, NgIf, MatCheckboxModule, MatTooltipModule],
+    imports: [MatCardModule, MatMenuModule, MatButtonModule, RouterLink, MatTableModule, NgIf, MatCheckboxModule, MatPaginatorModule],
     templateUrl: './notifications-page.component.html',
     styleUrl: './notifications-page.component.scss'
 })
 export class NotificationsPageComponent {
 
-    displayedColumns: string[] = ['notificationID', 'content', 'timestamp'];
-    dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-    selection = new SelectionModel<PeriodicElement>(true, []);
-}
+    displayedColumns: string[] = ['content', 'timestamp'];
+    dataSource = new MatTableDataSource<any>;
 
-const ELEMENT_DATA: PeriodicElement[] = [
-    {
-        notificationID: '#258',
-        timestamp: '01 Dec, 2023 09:30 AM',
-        type: 'Information',
-        content: 'New software update available. Click here to download.',
-        status: {
-            read: 'Read',
-            // unread: 'Unread',
-        }
-    },
-    {
-        notificationID: '#951',
-        timestamp: '30 Nov, 2023 09:30 AM',
-        type: 'Alert',
-        content: 'Urgent: Server maintenance scheduled for tomorrow at 10:00 PM.',
-        status: {
-            // read: 'Read',
-            unread: 'Unread',
-        }
-    },
-    {
-        notificationID: '#753',
-        timestamp: '28 Nov, 2023 09:30 AM',
-        type: 'Announcement',
-        content: 'Welcome to our new team member, Jane Smith, as Head of Sales.',
-        status: {
-            read: 'Read',
-            // unread: 'Unread',
-        }
-    },
-    {
-        notificationID: '#852',
-        timestamp: '27 Nov, 2023 09:30 AM',
-        type: 'Information',
-        content: 'Check out our latest blog post for tips on improving efficiency.',
-        status: {
-            read: 'Read',
-            // unread: 'Unread',
-        }
-    },
-    {
-        notificationID: '#486',
-        timestamp: '26 Nov, 2023 09:30 AM',
-        type: 'Alert',
-        content: 'Attention: Upcoming webinar on cybersecurity best practices.',
-        status: {
-            // read: 'Read',
-            unread: 'Unread',
-        }
-    },
-    {
-        notificationID: '#357',
-        timestamp: '25 Nov, 2023 09:30 AM',
-        type: 'Announcement',
-        content: 'Happy Thanksgiving! Our office will be closed on Nov 30.',
-        status: {
-            read: 'Read',
-            // unread: 'Unread',
-        }
-    },
-    {
-        notificationID: '#957',
-        timestamp: '24 Nov, 2023 09:30 AM',
-        type: 'Information',
-        content: 'Year-end financial reports are now available for download.',
-        status: {
-            read: 'Read',
-            // unread: 'Unread',
-        }
-    },
-    {
-        notificationID: '#153',
-        timestamp: '23 Nov, 2023 09:30 AM',
-        type: 'Announcement',
-        content: 'Critical security update. Update your passwords immediately.',
-        status: {
-            read: 'Read',
-            // unread: 'Unread',
-        }
-    },
-    {
-        notificationID: '#257',
-        timestamp: '22 Nov, 2023 09:30 AM',
-        type: 'Information',
-        content: 'Holiday Office Party on Dec 20. Save the date!',
-        status: {
-            // read: 'Read',
-            unread: 'Unread',
-        }
-    },
-    {
-        notificationID: '#352',
-        timestamp: '20 Nov, 2023 09:30 AM',
-        type: 'Alert',
-        content: 'Year-end sale: Up to 20% off on selected products.',
-        status: {
-            read: 'Read',
-            // unread: 'Unread',
-        }
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+
+    // isToggled
+    isToggled = false;
+
+    constructor(
+        public themeService: CustomizerSettingsService,
+        private http: HttpClient,
+        private dialog: MatDialog,
+        private apiSrv: APIService
+    ) {
+        this.themeService.isToggled$.subscribe(isToggled => {
+            this.isToggled = isToggled;
+        });
     }
-];
 
-export interface PeriodicElement {
-    notificationID: string;
-    timestamp: string;
-    type: string;
-    content: string;
-    status: any;
+    ngOnInit() {
+        this.fetchData();
+    }
+
+    fetchData() {
+        this.http.get<any[]>(this.apiSrv.URL + 'api/notifications')
+            .subscribe(data => {
+                data = data.reverse();
+                this.dataSource.data = data.map((item, index) => ({
+                    id: item._id,
+                    notificationID: (index + 1).toString().padStart(2, '0') + '.',
+                    content: { notificationName: item.statement },
+                    timestamp: {createdAt: item.createdAt}
+                }));
+                this.dataSource.paginator = this.paginator;
+            });
+    }
+
+
+    // RTL Mode
+    toggleRTLEnabledTheme() {
+        this.themeService.toggleRTLEnabledTheme();
+    }
 }
